75dc67030057c3857232bcce365dad8d63d297c6
Paches19 <pachees19@gmail.com>
2022-10-03 12:52:42 +0200

    atoi puntero

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 15:45:56 by adpachec          #+#    #+#             */
-/*   Updated: 2022/09/30 13:15:35 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/10/03 12:52:18 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 #include "libft.h"
 
@@ -20,22 +22,23 @@ int	ft_atoi(const char *str)
 
 	i = 0;
 	sign = 1;
-	while (str[i] && (str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
-		++i;
-	if (str[i] == '+' || str[i] == '-')
+	while (*str && (*str == 32 || (*str >= 9 && *str <= 13)))
+		++str;
+	if (*str == '+' || *str == '-')
 	{
-		if (str[i] == '-')
+		if (*str == '-')
 			sign = (-1);
-		++i;
+		++str;
 	}
 	result = 0;
-	while (ft_isdigit(str[i]))
+	while (ft_isdigit(*str))
 	{
-		result = (result * 10) + (str[i++] - '0') * sign;
+		result = (result * 10) + (*str - '0') * sign;
 		if (result > INT_MAX)
 			return (-1);
 		else if (result < INT_MIN)
 			return (0);
+		++str;
 	}
 	return (result);
 }
